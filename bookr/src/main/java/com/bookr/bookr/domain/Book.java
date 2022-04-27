9fecbe48a1a1ecf71ca2a12e5213697bd0d97729
marcitudev <mncosta99@gmail.com>
2022-04-26 22:00:48 -0300

    Domain classes created

---
+++ bookr/src/main/java/com/bookr/bookr/domain/Book.java
@@ -0,0 +1,80 @@
+package com.bookr.bookr.domain;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String text;
+    private Category category;
+    
+    public Book() {
+        super();
+    }
+
+    public Book(Long id, String title, String author, String text) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Book other = (Book) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+}
