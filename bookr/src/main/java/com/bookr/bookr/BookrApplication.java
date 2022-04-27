59cee5de9051f5c3395897509eed373e7526cb46
marcitudev <mncosta99@gmail.com>
2022-04-26 21:48:02 -0300

    Init backend project

---
+++ bookr/src/main/java/com/bookr/bookr/BookrApplication.java
@@ -0,0 +1,13 @@
+package com.bookr.bookr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookrApplication.class, args);
+	}
+
+}
