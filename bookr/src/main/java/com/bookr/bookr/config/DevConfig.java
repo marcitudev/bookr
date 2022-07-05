864c6a3d944717f681fe21ea7cf92bb061b13ff6
Márcio Costa <marcio.costa@virtus.ufcg.edu.br>
2022-07-05 00:31:21 -0300

    Dev profile added

--- bookr/src/main/java/com/bookr/bookr/config/DevConfig.java
+++ bookr/src/main/java/com/bookr/bookr/config/DevConfig.java
@@ -2,19 +2,26 @@ package com.bookr.bookr.config;
 
 import com.bookr.bookr.service.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     @Autowired
     private DBService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
     @Bean
-    public void dbInstance(){
-        this.dbService.dbInstance();
+    public boolean dbInstance(){
+        if(strategy.equals("create")){
+            dbService.dbInstance();
+        }
+        return false;
     }
 }
